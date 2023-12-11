ab0abe38f28d700623d01ba708377450e02073a6
Ivosha <ivandidik1990@gmail.com>
2023-12-11 13:16:58 +0300

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.slider').forEach((n, i) => {
+    window[`slider${i + 1}`] = new Swiper(n, {
+        freeMode: true,
+        centerSlides: true,
+        direction: 'vertical',
+        mousewheel: true,
+        slidesPreView: 1.75,
+        slidesOffsetBefore: -125
+    })
+})
+
+bindSwipers(slider1, slider2, slider3, slider4)
